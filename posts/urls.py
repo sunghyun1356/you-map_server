2dc084f1a284e6f33db3843946ab0eb62ea6300f
yuyi5187 <kimyy200063@gmail.com>
2023-08-03 11:28:43 +0900

    list, create api 분리

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('popular/', views.PurposeListView.as_view(), kwargs={'ordering': 'popular'}), 
     path('recent/', views.PurposeListView.as_view(), kwargs={'ordering': 'recent'}),
     path('<int:pk>', views.PostDetailAPIView.as_view()),
-    path('', views.PostListCreateAPIView.as_view()),
+    path('', views.PostListAPIView.as_view()),
+    path('', views.PostCreateAPIView.as_view()),
 ]
