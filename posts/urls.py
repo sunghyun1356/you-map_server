48537ea8ac437fb5da859eac85b821a424090efd
sunghyun1356 <sunghyun1356@naver.com>
2023-08-03 15:11:08 +0900

    무시

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('recent/', views.PurposeListView.as_view(), kwargs={'ordering': 'recent'}),
     path('<int:pk>', views.PostDetailAPIView.as_view()),
     path('', views.postListCreateCombined),
-    # path('', views.PostCreateAPIView.as_view()),
+    #path('', views.PostCreateAPIView.as_view()),
 ]
