ada5064206374588821cc1283b4e6c020c1a85fd
yuyi5187 <kimyy200063@gmail.com>
2023-08-03 11:07:41 +0900

    writerserializer 생성

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
