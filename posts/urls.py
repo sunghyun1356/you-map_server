0b107a482597b89e1a057faf4ea75e5cd03d8518
MyungJune <kevinshin123@gmail.com>
2023-08-21 16:39:10 +0900

    implement 2 post views with CursorPagination

--- posts/urls.py
+++ posts/urls.py
@@ -5,8 +5,8 @@ from . import views
 from likes import views as like_views
 
 urlpatterns = [
-    path('popular/', views.PurposeListView.as_view(), kwargs={'ordering': 'popular'}), 
-    path('recent/', views.PurposeListView.as_view(), kwargs={'ordering': 'recent'}),
+    path('popular', views.PopularFirstPostListAPIView.as_view()),
+    path('recent', views.RecentFirstPostListAPIView.as_view()),
     path('<int:pk>', views.PostDetailAPIView.as_view()),
     path('',views.PostCreateAPIView.as_view()),
 
